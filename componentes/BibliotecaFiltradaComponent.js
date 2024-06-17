960317cbd352ec3949b384330b358dbf82d79996
Adrian Amatriain <amatriain.133735@e.unavarra.es>
2024-06-17 19:14:13 +0200

    Valoraciones en biblioteca filtrada

--- componentes/BibliotecaFiltradaComponent.js
+++ componentes/BibliotecaFiltradaComponent.js
@@ -1,21 +1,24 @@
-import { Component, useEffect } from "react";
+import { Component } from "react";
 import { connect } from "react-redux";
-import { fetchLibros } from "../redux/actions/libros";
 import { ListItem } from "react-native-elements";
 import { colorAzulClaro } from "../app.config";
 import LibroSimple from "./LibroSimpleComponent";
 import { IndicadorActividad } from "./IndicadorActividadComponent";
 import { FlatList, SafeAreaView, StyleSheet, View } from "react-native";
 import { Text } from "react-native";
+import { fetchLibros } from "../redux/actions/libros";
+import { fetchComentariosValoracionMedia } from "../redux/actions/comentarios";
 
 const mapStateToProps = (state) => ({
   loading: state.libros.loading,
   error: state.libros.errMess,
   libros: state.libros.libros,
+  valoracionesMedias: state.comentarios.valoracionesMedias,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   fetchLibros: () => dispatch(fetchLibros()), //aqui cambiar por la que coge los libros segun el estado y el usuario
+  fetchComentariosValoracionMedia: (idLibro) => dispatch(fetchComentariosValoracionMedia(idLibro)),
 });
 
 class BibliotecaFiltrada extends Component {
@@ -40,11 +43,16 @@ class BibliotecaFiltrada extends Component {
         this.props.navigation.setOptions({ title: "Mis Discusiones" });
       }
     }
+    if (this.props.libros !== prevProps.libros && !this.props.loading) {
+      this.props.libros.forEach(libro => {
+        this.props.fetchComentariosValoracionMedia(libro.idLibro);
+      });
+    }
   }
 
   render() {
     const { navigate } = this.props.navigation;
-    const { libros, loading, error } = this.props;
+    const { libros, loading, error, valoracionesMedias } = this.props;
     const { estado } = this.props.route.params;
 
     const isDiscusion = estado === "Discusiones";
@@ -63,21 +71,23 @@ class BibliotecaFiltrada extends Component {
     }
 
     const renderBibliotecaItem = ({ item, index }) => {
+      const valoracionMedia = valoracionesMedias[item.idLibro];
+
       return (
         <ListItem
           containerStyle={styles.container}
           key={index}
           onPress={() => {
             if (isDiscusion) {
-              navigate("Biblioteca",{screen:"Discusion", params: { idLibro: item.idLibro }});
+              navigate("Biblioteca", { screen: "Discusion", params: { idLibro: item.idLibro } });
             } else {
               {
-                navigate("Biblioteca",{screen: "DetalleLibro", params: { idLibro: item.idLibro }});
+                navigate("Biblioteca", { screen: "DetalleLibro", params: { idLibro: item.idLibro } });
               }
             }
           }}
           bottomDivider>
-          <LibroSimple libro={item} mostrarValidacion={true} />
+          <LibroSimple libro={item} valoracionMedia={valoracionMedia !== undefined ? valoracionMedia : "0"} />
         </ListItem>
       );
     };
